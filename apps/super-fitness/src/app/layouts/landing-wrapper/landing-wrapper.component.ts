14661955dec012a01dbfac052534c2e2fc703e20
ahmed.gamal <ahmed.gamal@areebgroup.com>
2026-06-27 06:04:41 +0300

    feat: remove footer

--- apps/super-fitness/src/app/layouts/landing-wrapper/landing-wrapper.component.ts
+++ apps/super-fitness/src/app/layouts/landing-wrapper/landing-wrapper.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
-import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing-wrapper',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, FooterComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './landing-wrapper.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
